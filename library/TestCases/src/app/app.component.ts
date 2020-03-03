d5219e2916cb989eee6361334f94d9536626d029
Dennis Fiedler <dennis.fiedler@proximity.com>
2020-03-03 10:06:50 +0100

    docs: update github pages

--- library/TestCases/src/app/app.component.ts
+++ library/TestCases/src/app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component, ElementRef, OnInit } from '@angular/core';
 
 import * as Stickyfill from 'stickyfilljs';
+import { Router } from '@angular/router';
 
 @Component({
-    selector: 'app-root',
+    selector: 'uni-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-    constructor(private _host: ElementRef) {}
+    constructor(private _host: ElementRef, private router: Router) {
+        const path = localStorage.getItem('path');
+        if (path) {
+            localStorage.removeItem('path');
+            this.router.navigate([path]);
+        }
+    }
 
     ngOnInit() {
         Stickyfill.add(this._host.nativeElement.querySelector('header'));
