c742f7ce95c4fde2f374b9ef0a194b4a4a440c33
Dennis Fiedler <dennis.fiedler@proximity.com>
2020-03-04 16:34:17 +0100

    docs: include travis

--- library/ngx-file-uploader/src/lib/services/ngx-file-uploader.service.spec.ts
+++ library/ngx-file-uploader/src/lib/services/ngx-file-uploader.service.spec.ts
@@ -1,16 +1,20 @@
-// import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
 
-// import { NgxFileUploaderService } from './ngx-file-uploader.service';
+import { FileUploadService } from './file-upload.service';
 
-// describe('NgxFileUploaderService', () => {
-//   let service: NgxFileUploaderService;
+describe('NgxFileUploaderService', () => {
+    let service: FileUploadService;
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({});
-//     service = TestBed.inject(NgxFileUploaderService);
-//   });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientModule],
+            providers: [FileUploadService]
+        });
+        service = TestBed.inject(FileUploadService);
+    });
 
-//   it('should be created', () => {
-//     expect(service).toBeTruthy();
-//   });
-// });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+});
