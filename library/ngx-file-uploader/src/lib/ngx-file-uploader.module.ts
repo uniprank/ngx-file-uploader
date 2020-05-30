5add114526df6e663502be519e949d31bb553ccb
Lisandro Cobeaga <lisandro.cobeaga@inworx.com>
2020-05-30 02:25:03 -0300

    Changed importing path to fix undefined with AOT

--- library/ngx-file-uploader/src/lib/ngx-file-uploader.module.ts
+++ library/ngx-file-uploader/src/lib/ngx-file-uploader.module.ts
@@ -2,8 +2,12 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 
 import { TransferOptionsInterface } from './interfaces';
 import { FILE_UPLOADER_CONFIG, FileUploadService } from './services/file-upload.service';
-import { ImagePreviewDirective, FileSelectDirective, FileDropDirective, ProgressBarDirective } from './directives';
-import { FileSizePipe } from './pipes';
+
+import { FileSizePipe } from './pipes/file-size.pipe';
+import { FileDropDirective } from './directives/file-drop.directive';
+import { FileSelectDirective } from './directives/file-select.directive';
+import { ImagePreviewDirective } from './directives/img-preview.directive';
+import { ProgressBarDirective } from './directives/progress-bar.directive';
 
 const providers = [FileUploadService];
 const directives = [FileDropDirective, FileSelectDirective, ImagePreviewDirective, ProgressBarDirective];
